ef72cab808fb1c21c732606cd49531f990c1a11a
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-07-08 20:31:36 +1000

    finished home page

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,5 @@ export default {
   font-family: 'Crimson Text', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-body {
-  background-color:  #f0ddbf;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,33 @@
 <template>
-  <div>
-    <h1 class="title">{{ msg }}</h1>
-    <b>Safew Tech Labs</b> is a boutique software development company. We develop high quality,
-        well tested, high performing and scalable products for the web, Android and iOS.
+  <div class="columns">
+    <div class="column"></div>
+    <div class="column content is-medium">
+      <h1 class="title">{{ msg }}</h1>
+      <b>Safew Tech Labs</b> is a boutique software development company. We develop high quality,
+        well tested, high performing and scalable products for the web, Android and iOS. We are incredibly
+        proud of our work and do not believe in taking half measures.
+      <p>We are a group of global experts in design, development &amp; Ops. We're happy to work on related technologies if need be.
+        We're constantly upskilling ourselves to remain on top of our game. Some of the technologies
+        we can work with immediately are listed below.
+      </p>
+      <ul>
+        <li>Ruby</li>
+        <li>Rails</li>
+        <li>PostgreSQl</li>
+        <li>Elasticsearch</li>
+        <li>Vue JS</li>
+        <li>AWS and public cloud stack</li>
+        <li>Serverless</li>
+      </ul>
+      <p>We are currently working on 2 products:</p>
+      <ul>
+        <li><a href="https://comix.life" target="blank">Comix</a> - A marketplace for Indie comics</li>
+        <li><a href="https://getnotified.me" target="blank">Get Notified</a> - A full service notifications API.</li>
+      </ul>
+      <p>If you have any inquiries related to above projects or have any projects we can help with feel free to get in touch.</p>
+      <p><a href="mailto:saurabh@safew.tech?subject=Howdy!" target="blank">Saurabh Bhatia</a>, Founder</p>
+    </div>
+    <div class="column"></div>
   </div>
 </template>
 
