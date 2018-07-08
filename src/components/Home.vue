afa935471ccde784d08f4110022137bfe6c73625
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-07-08 18:35:02 +1000

    safew labs website

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="title">{{ msg }}</h1>
+    <b>Safew Tech Labs</b> is a boutique software development company. We develop high quality,
+        well tested, high performing and scalable products for the web, Android and iOS.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Safew Tech Labs'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
